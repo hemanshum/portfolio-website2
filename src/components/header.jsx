1347125a286c8575cfc80f2b1d214293b22e0f3f
Hemanshu <hemanshu.mahajan@gmail.com>
2024-05-20 13:39:57 +0530

    Hero Section completed

--- src/components/header.jsx
+++ src/components/header.jsx
@@ -1,29 +1,29 @@
-import Link from 'next/link';
-import { Button } from '@/components/ui/button';
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
 
-import Nav from './nav';
-import MobileNav from './mobile-nav';
+import Nav from "./nav";
+import MobileNav from "./mobile-nav";
 
 const Header = () => {
   return (
-    <header className='py-8 xl:py-12 text-white'>
-      <div className='container mx-auto flex justify-between items-center'>
+    <header className="py-8 xl:py-12 text-white">
+      <div className="container mx-auto flex justify-between items-center">
         {/* logo */}
-        <Link href='/'>
-          <h1 className='text-4xl font-semibold'>
-            Hemanshu<span className='text-accent'>.</span>
+        <Link href="/">
+          <h1 className="text-4xl font-black">
+            Eklauta<span className="text-accent">.</span>
           </h1>
         </Link>
         {/* desktop nav & hire me button */}
-        <div className='hidden xl:flex items-center gap-8'>
+        <div className="hidden xl:flex items-center gap-8">
           <Nav />
-          <Link href='/contact'>
+          <Link href="/contact">
             <Button>Hire me</Button>
           </Link>
         </div>
 
         {/* mobile nav */}
-        <dir className='xl:hidden'>
+        <dir className="xl:hidden">
           <MobileNav />
         </dir>
       </div>
